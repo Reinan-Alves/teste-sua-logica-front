bbde31275c8b0f292d0e9725a8aa969c3dd42fae
Reinan Alves <95976564+Reinan-Alves@users.noreply.github.com>
2023-07-22 02:53:16 -0300

    fechar menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonMenu } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild(IonMenu) menu: IonMenu;
   constructor() {}
+  fecharMenu() {
+    this.menu.close();
+  }
 }
