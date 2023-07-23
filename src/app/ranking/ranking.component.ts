32f307417c92a873cba96a8df0b5ef4b2e4a0b93
Reinan Alves <95976564+Reinan-Alves@users.noreply.github.com>
2023-07-22 23:15:35 -0300

    implementando o select no ranking

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -1,6 +1,6 @@
 import { RankingService } from './../service/ranking.service';
 import { Pontuacao } from './../model/potuacao.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-ranking',
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ranking.component.scss'],
 })
 export class RankingComponent implements OnInit {
+  categoriaEscolhida='raciocinio-logico';
   statusConexao = 'sucesso';
   public listaDeRanking: Array<Pontuacao> = [];
-  public rankingRaciocinioLogico: Array<Pontuacao> = [];
-  public rankingLogicaMatematica: Array<Pontuacao> = [];
   public listaDeCategoria: Array<string> = [];
+  public rakingDaCategoria: Array<Pontuacao> = [];
 
   constructor(private rankingService: RankingService) {}
 
@@ -26,8 +26,10 @@ export class RankingComponent implements OnInit {
       this.rankingService.listaDeRanking().subscribe({
         next: (res) => {
           this.listaDeRanking = res;
-          this.ordenarRanking();
+          console.log(this.listaDeRanking);
+          this.categoriaEscolhida = this.listaDeRanking[this.listaDeRanking.length-1].categoria;
           this.dividirCategoria();
+          this.ordenarRanking();
           this.enumerarPosicoes();
           this.statusConexao = 'sucesso';
         },
@@ -39,13 +41,24 @@ export class RankingComponent implements OnInit {
     }, 2000);
   }
 
-  // adicionarAoRanking(pontuacaoPessoal: Pontuacao) {
-  //   this.listaDeRanking.push(pontuacaoPessoal);
-  //   console.log('ok');
-  // }
+  dividirCategoria() {
+    // O set forma um array sem valores repetidos
+    const listaDeRankingSet = new Set(this.listaDeRanking.map((e) => e.categoria));
+    //motando uma lista das categorias existentes
+    this.listaDeCategoria = Array.from(listaDeRankingSet);
+    this.rakingDaCategoria = this.listaDeRanking.filter(
+      (e) => e.categoria === this.categoriaEscolhida
+    );
+  }
+
+  atualizarRankingCategoria() {
+    this.dividirCategoria();
+    this.ordenarRanking();
+    this.enumerarPosicoes();
+  }
 
   ordenarRanking() {
-    this.listaDeRanking = this.listaDeRanking.sort((i1, i2) => {
+    this.rakingDaCategoria = this.rakingDaCategoria.sort((i1, i2) => {
       if (i1.pontos < i2.pontos) {
         return 1;
       } else if (i1.pontos > i2.pontos) {
@@ -55,24 +68,10 @@ export class RankingComponent implements OnInit {
       }
     });
   }
-  dividirCategoria() {
-    // O set forma um array sem valores repetidos
-    const listaDeRankingSet = new Set(this.listaDeRanking.map((e) => e.categoria));
-    //motando uma lista das categorias existentes
-    this.listaDeCategoria = Array.from(listaDeRankingSet);
 
-    this.rankingLogicaMatematica = this.listaDeRanking.filter(
-      (e) => e.categoria === 'logica-matematica'
-    );
-    this.rankingRaciocinioLogico = this.listaDeRanking.filter(
-      (e) => e.categoria === 'raciocinio-logico'
-    );
-  }
   enumerarPosicoes() {
-    this.rankingRaciocinioLogico.forEach((e, index) => {
-      e.posicao = index + 1;
-    });
-    this.rankingLogicaMatematica.forEach((e, index) => {
+
+    this.rakingDaCategoria.forEach((e, index) => {
       e.posicao = index + 1;
     });
   }
